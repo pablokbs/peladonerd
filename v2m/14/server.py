2bf8e741fcc2487da3d63a9973ecc91cd248b694
Pablo Fredrikson <pablo.fredrikson@gmail.com>
2019-12-02 00:01:00 -0300

    Agregando v2m 14

---
+++ v2m/14/server.py
@@ -0,0 +1,9 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
