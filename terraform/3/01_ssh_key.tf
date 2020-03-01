8eb5976126568b20b1cbb29ac1bc8f84ca24c933
Miguel Ignacio Costa <imcosta@MacBook-Pro-de-Miguel.local>
2020-03-01 17:44:05 -0300

    README y prevent_default para volumen

--- terraform/3/01_ssh_key.tf
+++ terraform/3/01_ssh_key.tf
@@ -1,8 +1,8 @@
 #
 # Exportamos nuestra key SSH
 
-resource "digitalocean_ssh_key" "imcosta" {
-  name       = "imcosta"
+resource "digitalocean_ssh_key" "key" {
+  name       = "key"
   public_key = file("id_rsa.pub")
 }
 
--- terraform/3/03_volume.tf
+++ terraform/3/03_volume.tf
@@ -5,6 +5,9 @@ resource "digitalocean_volume" "mail" {
   size                    = 5
   initial_filesystem_type = "ext4"
   description             = "Email disk"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "digitalocean_volume_attachment" "mail" {
