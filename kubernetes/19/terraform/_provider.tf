243d1ec0e2951a1f0c50ee0d0fc2c26c0c11200f
Andrés Zacchino <azacchino@users.noreply.github.com>
2020-10-09 11:47:46 -0300

    Update _provider.tf
    Fix: 
    Could not find required providers, but found possible alternatives:
    
      hashicorp/digitalocean -> digitalocean/digitalocean

--- kubernetes/19/terraform/_provider.tf
+++ kubernetes/19/terraform/_provider.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "1.22.2"
+    }
+  }
+}
+
 variable "digitalocean_token" {}
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "${var.digitalocean_token}"
+  token = var.digitalocean_token
 }
 
