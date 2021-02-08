1ccdfe4e4abfae9a7905c3b6b65b058c23adf035
Pablo Fredrikson <pablo.fredrikson@gmail.com>
2021-02-07 23:40:20 -0300

    Agregando k0s

---
+++ kubernetes/29/boot_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "<token>" > /root/worker.token
+
+wget https://github.com/k0sproject/k0s/releases/download/v0.9.1/k0s-v0.9.1-amd64  -O /usr/local/bin/k0s 
+chmod +x /usr/local/bin/k0s
+k0s install --role worker
+sed -i 's/REPLACEME/\/root\/worker\.token/g' /etc/systemd/system/k0s.service
+systemctl daemon-reload
+systemctl start k0s
