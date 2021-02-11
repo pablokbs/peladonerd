a7393f46893ddbe45582fa3d3dabbfb7e7774a69
Julian Marcos <julian08@riseup.net>
2021-02-11 06:59:49 +0100

    Update sed  command
    TIP: You can use sed with another char as long as  the syntax is valid
    Also try to run it by yourself if you want

--- kubernetes/29/boot_script.sh
+++ kubernetes/29/boot_script.sh
@@ -5,7 +5,7 @@ echo "<token>" > /root/worker.token
 wget https://github.com/k0sproject/k0s/releases/download/v0.9.1/k0s-v0.9.1-amd64  -O /usr/local/bin/k0s 
 chmod +x /usr/local/bin/k0s
 k0s install --role worker
-sed -i 's/REPLACEME/\/root\/worker\.token/g' /etc/systemd/system/k0s.service
+sed -i 's|REPLACEME|/root/worker\.token|g' /etc/systemd/system/k0s.service
 systemctl daemon-reload
 systemctl enable k0s
 systemctl start k0s
