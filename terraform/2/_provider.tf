606af70c06bdb9dfaa67fadecbbc5f4774ac3087
lucabecci <beccibrian@gmail.com>
2021-10-17 21:47:32 -0300

    fix(): Failed to query available provider packages

--- terraform/2/_provider.tf
+++ terraform/2/_provider.tf
@@ -1,5 +1,13 @@
 variable "digitalocean_token" {}
 
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = "${var.digitalocean_token}"
